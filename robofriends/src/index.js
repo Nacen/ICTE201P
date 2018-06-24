1736efa07ea4641866a22e8e1ef03b2477f975f9
Vincent <overlordnacen@gmail.com>
2018-06-24 22:31:30 +0800

    Add App Component and Search Component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-import {robots} from './robots.js';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 ReactDOM.render(
-                <CardList robots = { robots }/>
+                <App />
                 , 
     document.getElementById('root'));
 registerServiceWorker();
