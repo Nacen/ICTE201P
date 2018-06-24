d62552c1487504677e49feb5b7b33ec13ad424f2
Vincent <overlordnacen@gmail.com>
2018-06-24 21:53:18 +0800

    Add CardList component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
+import {robots} from './robots.js';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <CardList robots = { robots }/>
+                , 
+    document.getElementById('root'));
 registerServiceWorker();
